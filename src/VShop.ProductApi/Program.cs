64a455df02c72c62fbc17539e3c86e37cef24ad1
PauloAlves8039 <pj38alves@gmail.com>
2024-01-16 22:18:11 -0300

    Criando contexto e aplicando migrations

--- src/VShop.ProductApi/Program.cs
+++ src/VShop.ProductApi/Program.cs
@@ -1,9 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using VShop.ProductApi.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+                  options.UseMySql(mySqlConnection,
+                    ServerVersion.AutoDetect(mySqlConnection)));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
